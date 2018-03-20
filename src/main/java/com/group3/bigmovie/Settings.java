ba90475f8cced5fbfdbda77a02aa5a6ea5986162
Pieter <pietergwestra@hotmail.com>
2018-03-20 10:46:49 +0100

    Updated gitignore and removed token from Settings.java

--- src/main/java/com/group3/bigmovie/Settings.java
+++ src/main/java/com/group3/bigmovie/Settings.java
@@ -3,5 +3,5 @@ package com.group3.bigmovie;
 
 public final class Settings
 {
-   public static final String BOT_TOKEN = "MzkzMDM5MDYxOTA2MDk2MTMx.DT4Nbw.qdAAp0BEG3qYluV3IcQasgITev0";
+   public static final String BOT_TOKEN = "INSERT TOKEN HERE";
 }
